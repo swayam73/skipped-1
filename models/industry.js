bbbbe72258c323ed4ad6ec1c5c025e0acd12d1e4
krisha <krisha.soni@minimaldot.com>
2021-08-04 16:30:07 +0530

    Database Models

--- models/industry.js
+++ models/industry.js
@@ -1,46 +1,4 @@
 
-let mysql = require("mysql");
-let dbconnection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "skipped"
-});
-
-dbconnection.connect((err) => {
-    if (err) {
-        return console.error("ERROR: " + err.message);
-    }
-    createindustry();
-});
-
-function createindustry() {
-    let sqlQuery = "CREATE DATABASE IF NOT EXISTS skipped";
-    dbconnection.query(sqlQuery, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        console.log("Database created...");
-    });
-}
-
-function createindustry() {
-    //Sets up the Database Table and Rows
-    const createTable = `CREATE TABLE IF NOT EXISTS industry
-    ( id int(11) NOT NULL,
-  name varchar(50) NOT NULL,
-  description varchar(50) NOT NULL,
-  status varchar(50) NOT NULL,
-  PRIMARY KEY (id))`;
-
-    //Execute tables creation query
-    dbconnection.query(createTable, (err, results, fields) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        console.log("Table and rows created...");
-    });
-}
-
 module.exports = function(sequelize, Sequelize) {
     var IndustrySchema = sequelize.define('Industry', {
         id: {
